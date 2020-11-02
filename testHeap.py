d0e0b07d5abc522fb2e9f984a00ca2f5da74b2fd
Taylor Witherell <witherellt21@attochron.com>
2020-11-02 09:34:37 -0500

    Add all files in

---
+++ testHeap.py
@@ -0,0 +1,37 @@
+"""
+File: testHeap.py
+YOUR NAME GOES HERE.
+"""
+
+
+from utils.linkedHeap import LinkedHeap
+from utils.arrayHeap import ArrayHeap
+import random
+
+
+def main(heapType):
+   lyst = list(range(1, 31))
+   random.shuffle(lyst)
+   
+   
+   hp = heapType()
+   
+   print("Adding items to heap, inspect for heap-ness:")
+   for item in lyst:
+      print("adding", item)
+      hp.add(item)
+      print(hp)
+      print()
+   
+   print("Heap size, expect 30:", len(hp))
+   
+   print("Expect items in order:", end=" ")
+   while not hp.isEmpty():
+      print(hp.pop(), end=" ")
+   print()
+   
+   print("Heap size, expect 0:", len(hp))
+   
+if __name__ == '__main__':
+   #main(ArrayHeap)
+   #main(LinkedHeap)
