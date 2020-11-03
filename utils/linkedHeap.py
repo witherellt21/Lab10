b40e4c835e7f411471d86af2a55f1232a65cabf3
witherellt21 <witherellt21@mail.wlu.edu>
2020-11-03 09:20:15 -0500

    Changed linkedHeap

--- utils/linkedHeap.py
+++ utils/linkedHeap.py
@@ -11,7 +11,7 @@ import math
 
 class LinkedHeap(AbstractHeap):
    """A link-based implementation of a heap."""
-   
+
    def __init__(self, sourceCollection = None):
       """Initialization of a heap."""
       self._heap = None
@@ -19,50 +19,70 @@ class LinkedHeap(AbstractHeap):
 
    def _getRoot(self):
       """Helper for AbstractHeap"""
-   
+
    def _getParent(self, node):
       """Helper for AbstractHeap"""
-   
+
    def _getLeftChild(self, node):
       """Helper for AbstractHeap"""
-   
+
    def _getRightChild(self, node):
       """Helper for AbstractHeap"""
-   
+
    def _getData(self, node):
       """Helper for AbstractHeap"""
 
    def _insideTree(self, node):
       """Helper for AbstractHeap"""
 
-   
+
    def _walkUp(self, node):
       """Walks node's data upwards through its parents while
          it is smaller than the parent."""
       pass
-   
+
    def _walkDown(self, node):
-      """Walks node's data upwards through its children while
-         it is larger than a child."""
-      pass
-   
+        """Walks node's data upwards through its children while
+        it is larger than a child."""
+        while node != None:
+            if node.left == None:
+                newnode = node.left
+                node.left = node
+                node = newnode
+                break
+            elif node.right == None:
+                newnode = node.right
+                node.right = node
+                node = newnode
+                break
+            elif node.data <= node.left.data:
+                newnode = node.left
+                node.left = node
+                node = newnode
+            elif node.data <= node.right.data:
+                newnode = node.right
+                node.right = node
+                node = newnode
+
+
+
 
    def add(self, item):
       """Adds item to the end of the array and then walks it up to the top,
          stopping when parent is less than the new item"""
       # increase size by one so that finding the last node points to
       #  the node BEYOND the last node
-      
+
       self._size += 1
       path = self._findPathToLastNode()
-      
+
       # Start at the top
       parentPointer = self._heap
-      
-      # If there is no path, we have an empty tree      
+
+      # If there is no path, we have an empty tree
       if len(path) == 0:
          self._heap = BSTNode(item)
-         
+
       # Otherwise, follow the path to one before the end
       else:
          # Use slicing to stop one early, could also use range(len(path) - 1)
@@ -72,36 +92,36 @@ class LinkedHeap(AbstractHeap):
                parentPointer = parentPointer.left
             else:
                parentPointer = parentPointer.right
-         
+
          # If our last direction choice is left, place new node there
          #  and walk it up the heap
          if path[-1] == "left":
             parentPointer.left = BSTNode(item, None, None, parentPointer)
             self._walkUp(parentPointer.left)
-         
+
          # Otherwise, right and walk
          else:
             parentPointer.right = BSTNode(item, None, None, parentPointer)
             self._walkUp(parentPointer.right)
-        
-      
+
+
    def pop(self):
       """Swaps the top element with the last element, then walks the top
          element down until both children are larger than the current node."""
       if self.isEmpty():
          raise KeyError("The heap is empty.")
-      
+
       # Find the path to the actual last node
       path = self._findPathToLastNode()
-      
+
       # Start at the top
       parentPointer = self._heap
-      
+
       # If there is no path, we have only one item in the heap
       if len(path) == 0:
          removedNode = self._heap
          self._heap = None
-      
+
       # Follow directions
       else:
          # Stop one early using slicing, could also use range(len(path)-1)
@@ -112,43 +132,42 @@ class LinkedHeap(AbstractHeap):
             # Go right
             else:
                parentPointer = parentPointer.right
-         
+
          # Replace the top based on the last choice, remove the last item
          if path[-1] == "left":
             removedNode = parentPointer.left
             removedNode.data, self._heap.data = self._heap.data, removedNode.data
             parentPointer.left = None
-            
+
          else:
             removedNode = parentPointer.right
             removedNode.data, self._heap.data = self._heap.data, removedNode.data
             parentPointer.right = None
-         
+
          # Walk down the new top item
          self._walkDown(self._heap)
-            
+
       # Decrease size now and return value popped
       self._size -= 1
-      
+
       return removedNode.data
-      
+
 
    def _findPathToLastNode(self):
       """Calculates the path to the last node in a linked
          heap based on the total number of items stored."""
-      
+
       n = len(self)
       path = []
-      
+
       total = math.floor(math.log(n, 2)) + 1
-      
+
       for i in range(total - 2, -1, -1):
          if (n // math.pow(2, i)) % 2 == 0:
             path.append("left")
          else:
             path.append("right")
-      
+
       # Return list of directions
       return path
 
-   
